d3ca8176dafa20f1c790967a3ed7f3c30a89c886
Le Ngoc Tram Anh <letramanh2182005@gmail.com>
2024-10-06 00:40:07 +0700

    Review_Js_Day_02

--- JavaScript/function.js
+++ JavaScript/function.js
@@ -1,18 +1,21 @@
 /**FUNCTION
  * 1.Hàm
- * -Một khối mã
+ * -Một khối lệnh
  * -Làm một cv cụ thể
+ * -nằm trong {}
  * 
  * 2.Loại hàm
  * -Build-in
  * -Tự định nghĩa
- * 3.Tính chất
  * 
+ * 
+ * 3.Tính chất
  * -ko thực thi khi định nghĩa
  * -sẽ thực thi khi đc gọi
  * -có thể nhận tham số
  * -trả về một gtri
- * 
+ * 5. C++ tạo ra node=> giúp js chạy trên mọi browser
+ * node:môi trường chạy js,đc cài sẵn trong mọi browser vì có v8
  * 4.Tạo hàm 
  */
 
@@ -28,28 +31,70 @@ handleInput()
  * 
  */
 
-const handleWriteLog=(e,i)=> {
-   console.log(e,i)
+// Các loại function
+//1.Function Declaration (Khai báo hàm):khai báo xong,lưu vào ô nhớ, có thể gọi hàm trc dòng khai báo
+addNumber(console.log(3,5))
+function addNumber(x,y) {
+    return x,y
 }
-languages.forEach(handleWriteLog) 
-/**Javascript
-PHP
-C++
-Go
-Ruby */
-handleWriteLog("Hello",123)// Hello 123 đối số
-handleWriteLog("Hello") //Hello undefined nếu có tham số nhưng ko truyền đối số tương ứng
-
-/** RETURN TRONG FUNCTION */
-const sum=(a,b)=> {
-    return (a+b) //6
-    console.log("he")
+//2. Function Expression (Biểu thức hàm):
+const square=function (num) {
+    return num*num;
 }
-/**- ko có return mặc định trả về undifined 
- * - dòng code sau return ko chạy nữa
- * - trả về bất cứ dữ liệu gì
-*/
-const result=sum(2,4)
-console.log(result)
+console.log(square(5))
+
+//3.Arrow Function
+const handleCount=(d,e)=> {
+return d+e
+}
+console.log(handleCount(3,5))
+
+// spread operator:khi muốn truyền nhiều đối số 
+const combineStrings=(...strings)=> {
+    return strings.join("")
+}
+console.log(combineStrings("Hi","World"))
+console.log(combineStrings("fsdvfs"))
+
+// RETURN {}: trả về object hoặc array vì giống,
+// nên return({}) hoặc return([]) để tránh nhầm lẫn block code
+const myObject={};
+const createUser=(name,age)=> {
+return ([
+    name=name,
+    age=age
+]) 
+}
+const user=createUser("TramAnh",25)
+console.log(user) //[ 'TramAnh', 25 ]
+
+const createUserContact=(phone,email)=> {
+    return ({phone:phone,
+        email:email})
+     }
+console.log(createUserContact("6555","gebg"))
+//{ phone: '6555', email: 'gebg' }
+
+//RETURN:trả về gtri đơn(string,number)
+const greet=(name)=>{
+    return "Hello"+ " "+ name+"!"
+}
+console.log(greet("fvfdb"))
 
 
+const createProduct=(...inf)=> {
+    const [id,brand,price]=inf
+    return  {   id, brand,price}
+}
+console.log(createProduct(1, "Laptop", 1500))
+
+//vì js tự động hiểu return là stop ko chạy nữa
+const createBook=(...info)=> {
+const [title,author,content]=info
+return
+{title,
+author,
+content
+}
+}
+console.log(createBook("dfvb","egre","ểg"))// undefined
