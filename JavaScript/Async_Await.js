c69845aab2f43dfd8dd6ab85a7b8d9bf7179b180
Le Ngoc Tram Anh <letramanh2182005@gmail.com>
2024-10-07 00:02:51 +0700

    Review-JS-Day-03

---
+++ JavaScript/Async_Await.js
@@ -0,0 +1,17 @@
+//ĐỒNG BỘ
+// const test=()=> {
+//     console.log("=====1====")
+//     console.log("=====2====")
+//     console.log("=====3====")
+// }
+// test()
+// =====1====
+// =====2====
+// =====3====
+
+const createIdProduct=()=> {
+    setTimeout(()=>console.log("1"))
+    console.log("2")
+    console.log("3")
+}
+createIdProduct()
