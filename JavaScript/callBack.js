bf3aa46e79c0f609214b0ef585077a87e974f079
Le Ngoc Tram Anh <letramanh2182005@gmail.com>
2024-10-07 15:55:39 +0700

    CallBack

--- JavaScript/callBack.js
+++ JavaScript/callBack.js
@@ -1,9 +1,16 @@
 /** CALLBACK 
- * 1. Là hàm
- * 2. Được truyền qua đối số 
+ * 1. Là hàm con
+ * 2. Được truyền như đối số của hàm cha
+ * 3.Hàm cha thực hiện thành công thì hàm con đc gọi lại 
+ * 4.Dùng để gọi api từ server/ các tác vụ ko đồng bộ(async)
 */
-const userNames=(user)=> {
-    console.log( "Value:", user
-    )
-    }
-    userNames(123)
+
+const doTask=(callback)=> {
+console.log("Làm bài tập")
+callback()
+}
+
+const afterTask=()=> {
+    console.log("Làm bài tập xong")
+}
+doTask(afterTask)
